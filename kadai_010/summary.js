7794c6d41a3186ba0c7b42c3812d18724c87accb
Gacky12 <jyss.ina0311@star4.gmobb.jp>
2024-07-06 05:31:47 +0900

    kadai_010

---
+++ kadai_010/summary.js
@@ -0,0 +1,22 @@
+$(function () {
+    
+    // change-color要素をクリックした時テキストの文字色を変更
+    $('#change-color').on('click', function () {
+        $('#target').css('color', 'orange');
+    });
+    
+    // change-textの要素をクリックした時テキスト内容を変更
+    $('#change-text').on('click', function() {
+        $('#target').text('調子はいかがですか？');
+    });
+
+    // fade-outの要素をクリックした時フェードアウトを実行
+    $('#fade-out').on('click', function() {
+        $('#target').fadeOut(1000);
+    });
+
+    // fade-inの要素をクリックした時フェードアウトを実行
+    $('#fade-in').on('click', function() {
+        $('#target').fadeIn(1000);
+    });
+});
